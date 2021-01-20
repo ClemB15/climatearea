c9dc2baf12248d319f98c8ab499e6dbf2e749193
ClemB15 <38423940+ClemB15@users.noreply.github.com>
2021-01-20 10:48:07 +0100

    Add files via upload

---
+++ netatmo_API/Examples/Config.php
@@ -0,0 +1,6 @@
+<?php
+$client_id = "5ff42bc1bf033435e56a85a0";
+$client_secret = "h431SW9QSQRNId4qC6FdYoZjjmBLMxIIYTfWP6ON";
+$test_username = "clebourbon@yahoo.fr";
+$test_password = "Cleemeent1997";
+?>
